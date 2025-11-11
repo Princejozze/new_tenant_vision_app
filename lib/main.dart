b64a7fb375456559dea2514b68d567cb32093ed6
Gift <giftj964@gmail.com>
2025-11-11 11:58:39 +0300

    feat(firebase): integrate Firebase (core + Firestore), apply Google Services plugin, add firebase_options.dart and google-services.json; fix receipt_service syntax; wire initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/src/navigation/router.dart';
 import 'package:myapp/src/services/house_service.dart';
 import 'package:myapp/src/services/theme_service.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
